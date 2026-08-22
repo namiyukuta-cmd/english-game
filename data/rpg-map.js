52453e5ce6cc9f4cec341f4035f4318b558d244d
namiyukuta-cmd <namiyukuta@gmail.com>
2026-08-22 14:27:55 +0900

    Add base RPG map data

---
+++ data/rpg-map.js
@@ -0,0 +1,27 @@
+window.RPG_MAP = {
+  width: 800,
+  height: 600,
+  playerStart: { x: 400, y: 330 },
+  roads: [
+    { x: 0, y: 270, width: 800, height: 90 },
+    { x: 350, y: 0, width: 100, height: 600 }
+  ],
+  water: [
+    { x: 610, y: 70, width: 135, height: 105 }
+  ],
+  houses: [
+    { x: 110, y: 100, width: 120, height: 95 },
+    { x: 535, y: 385, width: 135, height: 105 }
+  ],
+  trees: [
+    { x: 70, y: 420 }, { x: 130, y: 465 }, { x: 210, y: 430 },
+    { x: 690, y: 250 }, { x: 735, y: 310 }, { x: 685, y: 500 },
+    { x: 285, y: 80 }, { x: 505, y: 90 }
+  ],
+  npcs: [
+    { id: 'villager_01', x: 280, y: 305, name: '村人' }
+  ],
+  enemies: [
+    { id: 'slime_01', x: 590, y: 300, name: 'スライム' }
+  ]
+};
