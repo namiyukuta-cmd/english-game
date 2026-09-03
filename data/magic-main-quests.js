acc81dc2886490731f632961c8261843aaf716c3
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-03 20:25:19 +0900

    Add Magic School main quest data

---
+++ data/magic-main-quests.js
@@ -0,0 +1,23 @@
+(()=>{
+  'use strict';
+
+  // Main quest data only. Change requirements here without rewriting dungeon logic.
+  window.MAGIC_MAIN_QUESTS={
+    1:{
+      id:'open_b2',
+      floor:1,
+      nextFloor:2,
+      titleEn:'Open the way to B2F',
+      titleJa:'B2Fへの道を開け',
+      requirements:[
+        {
+          type:'discover',
+          floor:1,
+          count:8,
+          labelEn:'Explore all 8 areas on B1F',
+          labelJa:'B1Fの8マスをすべて発見する'
+        }
+      ]
+    }
+  };
+})();
