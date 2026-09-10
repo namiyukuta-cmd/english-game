38fc3afedf4b85c52744e1e65b65c812b0fe52cb
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-10 17:15:35 +0900

    Open latest Desert Survival pickup field

--- desertsurvival/js/index.js
+++ desertsurvival/js/index.js
@@ -9,7 +9,7 @@ import {
   setActiveGame
 } from './save.js';
 
-const FIELD_URL = './desertsurvival_field.html?v=20260910-fixed3d1';
+const FIELD_URL = './desertsurvival_field.html?v=20260910-pickup1';
 
 const newGameBtn = document.getElementById('newGameBtn');
 const continueBtn = document.getElementById('continueBtn');
