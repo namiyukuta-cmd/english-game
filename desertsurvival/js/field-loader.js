1b637514b1e4477ebee333d42ac600595be7e8c9
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-11 00:55:03 +0900

    Refresh field after water point placement update

--- desertsurvival/js/field-loader.js
+++ desertsurvival/js/field-loader.js
@@ -5,7 +5,7 @@ const game = getActiveGame();
 if (!game) {
   location.replace('./desertsurvival_index.html');
 } else {
-  await import('./field3d.js?v=20260911-waterpoints1');
+  await import('./field3d.js?v=20260911-waterpoints2');
   await import('./field-ui.js?v=20260910-fieldlayout1');
   await import('./map-ui.js?v=20260910-worldmap1');
   await import('./hotbar.js?v=20260910-inventorylayout1');
