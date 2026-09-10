78a253b6ae109bae7b5102db0572674f27c27934
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-10 20:33:24 +0900

    Route Desert Survival to cache-safe field v2

--- desertsurvival/js/index.js
+++ desertsurvival/js/index.js
@@ -9,7 +9,7 @@ import {
   setActiveGame
 } from './save.js';
 
-const FIELD_URL = './desertsurvival_field.html?v=20260910-topui2';
+const FIELD_URL = './desertsurvival_field_v2.html?v=20260910-topui3';
 
 const newGameBtn = document.getElementById('newGameBtn');
 const continueBtn = document.getElementById('continueBtn');
