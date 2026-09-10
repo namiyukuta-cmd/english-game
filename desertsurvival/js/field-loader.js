a13a41338e63662248de09b0aa52df5c16ef490f
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-11 06:01:45 +0900

    Load anchored water-source controls

--- desertsurvival/js/field-loader.js
+++ desertsurvival/js/field-loader.js
@@ -8,5 +8,6 @@ if (!game) {
   await import('./field3d.js?v=20260911-waterdrink2');
   await import('./field-ui.js?v=20260910-fieldlayout1');
   await import('./map-ui.js?v=20260910-worldmap1');
-  await import('./hotbar.js?v=20260911-waterdrink2');
+  await import('./hotbar.js?v=20260911-waterbutton1');
+  await import('./water-ui.js?v=20260911-waterbutton1');
 }
