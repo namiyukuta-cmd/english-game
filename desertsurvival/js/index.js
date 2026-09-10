0eb759afa8bcf5f69e59e8e39fe2662c511bc297
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-10 18:59:27 +0900

    Refresh Desert Survival field cache version

--- desertsurvival/js/index.js
+++ desertsurvival/js/index.js
@@ -9,7 +9,7 @@ import {
   setActiveGame
 } from './save.js';
 
-const FIELD_URL = './desertsurvival_field.html?v=20260910-pickup1';
+const FIELD_URL = './desertsurvival_field.html?v=20260910-hud1';
 
 const newGameBtn = document.getElementById('newGameBtn');
 const continueBtn = document.getElementById('continueBtn');
