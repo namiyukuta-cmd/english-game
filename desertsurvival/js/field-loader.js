7215d8153b5a8dde677d206eff09fc91f4555d8a
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-10 22:09:17 +0900

    Guard Desert Survival field state before loading gameplay

---
+++ desertsurvival/js/field-loader.js
@@ -0,0 +1,12 @@
+import { getActiveGame } from './save.js?v=20260910-stateprotect1';
+
+const game = getActiveGame();
+
+if (!game) {
+  location.replace('./desertsurvival_index.html');
+} else {
+  await import('./field3d.js?v=20260910-itemuse1');
+  await import('./field-ui.js?v=20260910-fieldlayout1');
+  await import('./map-ui.js?v=20260910-worldmap1');
+  await import('./hotbar.js?v=20260910-itemuse1');
+}
