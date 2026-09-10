320c93031221fa3a1da6c296cfd5c7935f715f25
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-10 19:56:34 +0900

    Point desert survival entry to refined HUD

--- desertsurvival/js/index.js
+++ desertsurvival/js/index.js
@@ -9,7 +9,7 @@ import {
   setActiveGame
 } from './save.js';
 
-const FIELD_URL = './desertsurvival_field.html?v=20260910-hudsketch2';
+const FIELD_URL = './desertsurvival_field.html?v=20260910-hud3';
 
 const newGameBtn = document.getElementById('newGameBtn');
 const continueBtn = document.getElementById('continueBtn');
