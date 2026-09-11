e19e1df4d31f99d6253ba6c9e86080163759643a
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-11 11:16:52 +0900

    Add 2D house building item data

---
+++ housebuilding/js/housebuilding_平面_アイテム.js
@@ -0,0 +1,16 @@
+window.HOUSEBUILDING_2D_ITEMS = [
+  {
+    id: "item_001",
+    name: "焚き火",
+    height: 1,
+    width: 1,
+    icon: "🔥"
+  },
+  {
+    id: "item_002",
+    name: "ベッド",
+    height: 1,
+    width: 2,
+    icon: "🛏️"
+  }
+];
