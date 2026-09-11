651241f1c605762e2cd975d1930665ea5bd8635b
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-11 20:04:02 +0900

    Add shop area v2 navigation

---
+++ housebuilding/js/フィールド_店エリア_v2.js
@@ -0,0 +1,7 @@
+document.getElementById('toBuildMap')?.addEventListener('click', () => {
+  location.href = './housebuilding_平面.html';
+});
+
+document.getElementById('toNextMap')?.addEventListener('click', () => {
+  location.href = './フィールド.html';
+});
