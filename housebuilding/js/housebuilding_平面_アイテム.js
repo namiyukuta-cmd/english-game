31126c510c3f00a9550d032a1b3b23a6abbf7721
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-11 17:02:44 +0900

    Add building item stock and purchase data

--- housebuilding/js/housebuilding_平面_アイテム.js
+++ housebuilding/js/housebuilding_平面_アイテム.js
@@ -2,29 +2,49 @@ window.HOUSEBUILDING_2D_ITEMS = [
   {
     id: "item_000",
     name: "ブロック",
+    category: "建設",
     height: 1,
     width: 1,
-    icon: "■"
+    icon: "■",
+    owned: 16,
+    price: 10,
+    purchasable: true,
+    unlocked: true
   },
   {
     id: "item_001",
     name: "焚き火",
+    category: "建設",
     height: 1,
     width: 1,
-    icon: "🔥"
+    icon: "🔥",
+    owned: 1,
+    price: 50,
+    purchasable: true,
+    unlocked: true
   },
   {
     id: "item_002",
     name: "ベッド",
+    category: "建設",
     height: 1,
     width: 2,
-    icon: "🛏️"
+    icon: "🛏️",
+    owned: 1,
+    price: 150,
+    purchasable: true,
+    unlocked: true
   },
   {
     id: "item_003",
     name: "ドア",
+    category: "建設",
     height: 2,
     width: 1,
-    icon: "🚪"
+    icon: "🚪",
+    owned: 0,
+    price: 100,
+    purchasable: true,
+    unlocked: true
   }
 ];
