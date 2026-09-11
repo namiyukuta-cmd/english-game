1d006118bd38cdcfc7a34cc81a3660ef395a20b2
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-11 20:02:32 +0900

    Wire shop area right navigation

--- housebuilding/js/フィールド_店エリア.js
+++ housebuilding/js/フィールド_店エリア.js
@@ -1,7 +1,12 @@
 // 店がある外フィールド。
 // 左端をタップすると家マップへ戻る。
+// 右端をタップすると次のフィールドへ進む。
 // 各店舗への入口は、このマップ上に後から追加する。
 
 document.getElementById('toHouse')?.addEventListener('click', () => {
   location.href = './フィールド_ハウス.html';
+});
+
+document.getElementById('toNextField')?.addEventListener('click', () => {
+  location.href = './フィールド.html';
 });
