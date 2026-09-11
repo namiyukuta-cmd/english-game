523f643f32ef440ae4c3bcca8279d5ad7174d792
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-11 11:39:23 +0900

    Add block and door to 2D housebuilding items

--- housebuilding/js/housebuilding_平面_アイテム.js
+++ housebuilding/js/housebuilding_平面_アイテム.js
@@ -1,5 +1,12 @@
 window.HOUSEBUILDING_2D_ITEMS = [
   {
+    id: "item_000",
+    name: "ブロック",
+    height: 1,
+    width: 1,
+    icon: "■"
+  },
+  {
     id: "item_001",
     name: "焚き火",
     height: 1,
@@ -12,5 +19,12 @@ window.HOUSEBUILDING_2D_ITEMS = [
     height: 1,
     width: 2,
     icon: "🛏️"
+  },
+  {
+    id: "item_003",
+    name: "ドア",
+    height: 2,
+    width: 1,
+    icon: "🚪"
   }
 ];
