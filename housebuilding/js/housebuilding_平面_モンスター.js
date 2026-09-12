1b16b04c785f395cb92aba198e142840f9d4ea50
namiyukuta-cmd <namiyukuta@gmail.com>
2026-09-12 18:43:31 +0900

    Add 2D housebuilding monster data

---
+++ housebuilding/js/housebuilding_平面_モンスター.js
@@ -0,0 +1,54 @@
+window.HOUSEBUILDING_2D_MONSTERS = [
+  {
+    id: "monster_000",
+    name: "洞窟ネズミ",
+    category: "モンスター",
+    icon: "🐀",
+    hp: 12,
+    maxHp: 12,
+    attack: 3,
+    defense: 0,
+    exp: 4,
+    area: "洞窟",
+    unlocked: true
+  },
+  {
+    id: "monster_001",
+    name: "洞窟コウモリ",
+    category: "モンスター",
+    icon: "🦇",
+    hp: 10,
+    maxHp: 10,
+    attack: 4,
+    defense: 0,
+    exp: 5,
+    area: "洞窟",
+    unlocked: true
+  },
+  {
+    id: "monster_002",
+    name: "スライム",
+    category: "モンスター",
+    icon: "🟢",
+    hp: 18,
+    maxHp: 18,
+    attack: 4,
+    defense: 1,
+    exp: 7,
+    area: "洞窟",
+    unlocked: true
+  },
+  {
+    id: "monster_003",
+    name: "ゴブリン",
+    category: "モンスター",
+    icon: "👺",
+    hp: 26,
+    maxHp: 26,
+    attack: 6,
+    defense: 2,
+    exp: 12,
+    area: "洞窟",
+    unlocked: true
+  }
+];
